dd4d31e1a2fde14bbfb82e60f5613fa32e6ec4c3
Jan Was <jan.was@starburstdata.com>
2022-04-06 12:45:20 +0200

    Bump dependencies version

--- src/main/resources/archetype-resources/docker-build.sh
+++ src/main/resources/archetype-resources/docker-build.sh
@@ -4,15 +4,15 @@ set -euo pipefail
 set -x
 
 if [ -f release.properties ]; then
-    VERSION=\$(grep 'project.rel.$groupId\\:$artifactId=' release.properties | cut -d'=' -f2)
+    VERSION=$(grep 'project.rel.$groupId\\:$artifactId=' release.properties | cut -d'=' -f2)
 else
-    VERSION=\$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+    VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 fi
-TAG=$githubRepo:\$VERSION
+TAG=$githubRepo:$VERSION
 
 docker buildx build \
     --platform linux/amd64,linux/arm64 \
     -t "$TAG" \
-    --build-arg VERSION="\$VERSION" \
+    --build-arg VERSION="$VERSION" \
     --push \
     .
